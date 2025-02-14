26979e763b707bf8dce1fed528910398a652a3c4
Lark Sigmuond A. Babao <129244163+Snorlark@users.noreply.github.com>
2025-02-15 03:35:42 +0800

    Add files via upload

---
+++ Mobile Programming/CurrentNewsApp/app/src/main/java/com/lark/currentnewsapp/MainActivity.java
@@ -0,0 +1,89 @@
+package com.lark.currentnewsapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.progressindicator.LinearProgressIndicator;
+import com.kwabenaberko.newsapilib.NewsApiClient;
+import com.kwabenaberko.newsapilib.models.Article;
+import com.kwabenaberko.newsapilib.models.request.EverythingRequest;
+import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<Article> articleList = new ArrayList<>();
+    NewsRecyclerAdapter adapter;
+    LinearProgressIndicator progressIndicator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.news_recycler_view);
+        progressIndicator = findViewById(R.id.progress_bar);
+
+        setupRecyclerView();
+        getNews();
+    }
+
+    void getNews() {
+        changeInProgress(true);
+        NewsApiClient newsApiClient = new NewsApiClient("d8f591e93e3a4d6f95184b3ceae41f26");
+        newsApiClient.getEverything(
+                new EverythingRequest.Builder()
+                        .q("news")
+                        .language("en")
+                        .build(),
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+                        runOnUiThread(()->{
+                            changeInProgress(false);
+                            articleList = response.getArticles();
+                            adapter.updateData(articleList);
+                            adapter.notifyDataSetChanged();
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.i("GOT FAILURE", Objects.requireNonNull(throwable.getMessage()));
+                    }
+                }
+        );
+    }
+
+    void setupRecyclerView() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new NewsRecyclerAdapter(articleList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    void changeInProgress(boolean show) {
+        if (show) {
+            progressIndicator.setVisibility(View.VISIBLE);
+        }
+        else {
+            progressIndicator.setVisibility(View.INVISIBLE);
+        }
+    }
+
+
+
+}
